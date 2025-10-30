fa862e0d181f10a46c6ca68f34e1b64f85db5319
veggiemania <milkcowgames@gmail.com>
2025-10-30 19:33:53 +0100

    Añadida la animación de destrucción de la calabaza

--- Assets/___Albert/Scripts/Objetos/CalabazaDestructible.cs
+++ Assets/___Albert/Scripts/Objetos/CalabazaDestructible.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class CalabazaDestructible : MonoBehaviour
 {
+    [SerializeField] private GameObject vfxPrefab;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bala"))
         {
+            // Reproducir sonido
             if (SoundManager.Instance != null)
                 SoundManager.Instance.PlaySound("DestruirCalabaza");
 
+            // Instanciar el VFX en la posición de la calabaza
+            if (vfxPrefab != null)
+            {
+                GameObject vfx = Instantiate(vfxPrefab, transform.position, Quaternion.identity);
+                Destroy(vfx, 2.0f); // Destruye el VFX después de 2 segundos
+            }
+
+            // Destruir la calabaza
             Destroy(gameObject);
         }
     }
