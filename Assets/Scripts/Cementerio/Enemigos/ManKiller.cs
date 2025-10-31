4729a36ef2e5dd523ce02a7ebf5d53f46ce26d83
FelixCalvo <fcalvoi@uoc.edu>
2025-10-31 19:32:45 +0100

    Creamos algoritmos de busqueda y alerta enemmigo. Cambiamos location de player para pruebas.

--- Assets/Scripts/Cementerio/Enemigos/ManKiller.cs
+++ Assets/Scripts/Cementerio/Enemigos/ManKiller.cs
@@ -4,16 +4,27 @@ using UnityEngine.AI;
 public class ManKiller : MonoBehaviour
 {
 
-private GameObject fpsController;
+    private GameObject fpsController;
+    private float distancia;
+    //distancia para que el enemigo se active y persiga al jugador
+    private float distanciaAlerta = 10.0f;
 
     void Start()
     {
+        //instancia del jugador
         fpsController = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.GetComponent<NavMeshAgent>().SetDestination(fpsController.transform.position);
-    }
+        //calculamos distancia entre el enemigo y el jugador
+        distancia = Vector3.Distance(this.gameObject.transform.position, fpsController.transform.position);
+        //print("Distancia: " + distancia);
+        if(distancia < distanciaAlerta)
+        {
+            //el enemigo se mueve hacia el jugador según la distancia puesta
+            this.gameObject.GetComponent<NavMeshAgent>().SetDestination(fpsController.transform.position);
+        }
+   }
 }
