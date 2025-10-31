fa937e2df93637693d2001bfa3f95a2e881a1abe
FelixCalvo <fcalvoi@uoc.edu>
2025-10-31 20:55:49 +0100

    Reparamos WalkingMan

--- Assets/Scripts/Cementerio/Enemigos/ManKiller.cs
+++ Assets/Scripts/Cementerio/Enemigos/ManKiller.cs
@@ -5,9 +5,15 @@ public class ManKiller : MonoBehaviour
 {
 
     private GameObject fpsController;
+    //distancia al jugador
     private float distancia;
+    [Header("Ajustes ManKiller")]
     //distancia para que el enemigo se active y persiga al jugador
+    [SerializeField]
     private float distanciaAlerta = 10.0f;
+    //velocidad al andar de ManKiller
+    [SerializeField]
+    private float velocidadManKillerAndando = 3.5f;
 
     void Start()
     {
@@ -21,12 +27,21 @@ public class ManKiller : MonoBehaviour
         //calculamos distancia entre el enemigo y el jugador
         distancia = Vector3.Distance(this.gameObject.transform.position, fpsController.transform.position);
         //print("Distancia: " + distancia);
-        if(distancia < distanciaAlerta)
+        if (distancia < distanciaAlerta)
         {
             //cambiar animacion para que entre el caminar
-            this.gameObject.GetComponent<Animator>().SetBool("Walking_ManKiller", true);
+            this.gameObject.GetComponent<Animator>().SetFloat("Walking_ManKiller", 1.0f);
             //el enemigo se mueve hacia el jugador según la distancia puesta
             this.gameObject.GetComponent<NavMeshAgent>().SetDestination(fpsController.transform.position);
+            //ajustamos velocidad del enemigo
+            this.gameObject.GetComponent<NavMeshAgent>().speed = velocidadManKillerAndando;
+        }
+        else
+        {
+            //cambiar animacion para que entre el idle
+            this.gameObject.GetComponent<Animator>().SetFloat("Walking_ManKiller", 0.0f);
+            //ponemos a cero la velocidad del enemigo
+            this.gameObject.GetComponent<NavMeshAgent>().speed = 0.0f;
         }
    }
 }
