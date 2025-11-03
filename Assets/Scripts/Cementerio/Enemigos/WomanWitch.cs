78c7801918c52175e9fdeaf10f92daa290bdfb35
FelixCalvo <fcalvoi@uoc.edu>
2025-11-03 11:19:43 +0100

    Hacemos seguimiento player

--- Assets/Scripts/Cementerio/Enemigos/WomanWitch.cs
+++ Assets/Scripts/Cementerio/Enemigos/WomanWitch.cs
@@ -61,14 +61,16 @@ public class WomanWitch : MonoBehaviour
     {
         if (!bloquearEnemigoMuerto)
         {
-            /*
-            //calculamos distancia entre el enemigo y el jugador
-            distancia = Vector3.Distance(this.gameObject.transform.position, fpsController.transform.position);
 
             //posicion del jugador pero con la y del enemigo para que no se incline al mirar
             posFPS = new Vector3(fpsController.transform.position.x, this.gameObject.transform.position.y, fpsController.transform.position.z);
             //Miramos siempre al jugador
             this.gameObject.transform.LookAt(posFPS);
+            /*
+            //calculamos distancia entre el enemigo y el jugador
+            distancia = Vector3.Distance(this.gameObject.transform.position, fpsController.transform.position);
+
+
             if (bloquearAtaque == false)
             {
                 if (distancia < 4.0f)
